22b4a8f348bb555fda7009a66eb38f6e430a9d09
Craig Wermert <craig@wermert.org>
2012-09-11 17:22:44 +0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Babybragr::Application.config.secret_token = '9a2a06814169729953c0f9cb889b5ea4ccdc8f632cdb1aaf777c7e598470c55488beac2632ea3d7e3b600b02622627ff343a4b9cb07eb8e7e607644705167e35'
